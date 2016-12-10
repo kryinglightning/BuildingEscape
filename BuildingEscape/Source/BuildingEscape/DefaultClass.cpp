e2e400b27c8f9a9d3c8a5fb5463086610cd6fd9f
Daria <gor.daria1@yandex.ru>
2016-12-10 20:23:25 +0300

    Initial Commit

---
+++ BuildingEscape/Source/BuildingEscape/DefaultClass.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "DefaultClass.h"
+
+DefaultClass::DefaultClass()
+{
+}
+
+DefaultClass::~DefaultClass()
+{
+}
