930087707cccc5c940b1f1207b09c66f3786daaa
Daria <gor.daria1@yandex.ru>
2016-12-21 11:33:35 +0300

    21-12 Fixed the crash, now collision logging out correctly

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -30,6 +30,46 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
 
-	// ...
+	//get player viewpoint this tick - where they're looking
+	FVector FPlayerViewpointLocation;
+	FRotator FPlayerViewpointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT FPlayerViewpointLocation,
+		OUT FPlayerViewpointRotation
+	);
+
+	///Draw a red trace in the world to visualize
+	FVector LineTraceEnd = FPlayerViewpointLocation + FPlayerViewpointRotation.Vector() * Reach;
+	DrawDebugLine(
+		GetWorld(),
+		FPlayerViewpointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
+
+	//Set up query params
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	///Linetrace (ray-cast) out to reach distance
+	FHitResult LineTraceHit;
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT LineTraceHit,
+		FPlayerViewpointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+
+	/// See what what we hit
+	AActor* ActorHit = LineTraceHit.GetActor();
+	if (ActorHit)
+		{
+		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()))
+		}
 }
 
